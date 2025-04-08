0ff6a68922ef05dec5ed27d0cc63b93e7c151828
SANIYA-MAHEVEEN <saniyamaheveen@gmail.com>
2025-04-09 00:01:09 +0530

    commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,24 +1,21 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-//import FishStockList from './inventoryManagement/FishStockList'
-import DiseasePrediction from './QualityAssurance/Prediction'
-import NavigationBar from './components/Navbar'
-//import AuthenticationPage from './Authentication/Authenticate'
-import LoginSignup from './Authentication/LoginSignup'
+import { Routes, Route } from "react-router-dom";
+import NavigationBar from './components/Navbar';
+import DiseasePrediction from './QualityAssurance/Prediction';
+import LoginSignup from './Authentication/LoginSignup';
 import Register from './Authentication/Register';
+
 const App = () => {
   return (
     <>
-    <NavigationBar />
-      {/*< FishStockList/>*/}
+      <NavigationBar />
       <DiseasePrediction />
-      {/*<AuthenticationPage />*/}
       <Routes>
-        <Route path='LoginSignup' elements={<LoginSignup/>}/>
-        <Route path='Register' elements={<Register/>} />
+        
+        <Route path="/LoginSignup" element={<LoginSignup />} />
+        <Route path="/Register" element={<Register />} />
       </Routes>
-      
-      </>
+    </>
   );
 };
 
--- frontend/src/Authentication/Register.jsx
+++ frontend/src/Authentication/Register.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import { Link } from "react-router-dom"; 
 import { BiUser } from "react-icons/bi";
 import { AiOutlineUnlock } from "react-icons/ai";
-import { Link } from "react-router-dom"; 
+
 
 const Register = () => {
   return (
